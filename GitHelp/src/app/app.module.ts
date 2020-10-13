b236836b3b950928957f654f562c44b61f923213
Jupshaw97 <70708707+Jupshaw97@users.noreply.github.com>
2020-10-13 16:37:36 -0400

    angular folder should work now

---
+++ GitHelp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContractorsComponent } from './contractors/contractors.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ContractorListComponent } from './contractor-list/contractor-list.component';
+import { ContractorProfileComponent } from './contractor-profile/contractor-profile.component';
+import { ClientProfileComponent } from './client-profile/client-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientsComponent,
+    ContractorsComponent,
+    SignUpFormComponent,
+    LoginFormComponent,
+    ClientListComponent,
+    ContractorListComponent,
+    ContractorProfileComponent,
+    ClientProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
